d523c4895075edc2f24389311d56dadd241337e4
jashanbansal0708 <jashanbansal7856@yahoo.com>
2019-06-06 02:11:08 +0530

    Add the responsiveness for mobile devices

--- main.js
+++ main.js
@@ -7,14 +7,17 @@ window.addEventListener("load", (result) => {
     
     let locationTimezone = document.querySelector('.location-timezone');
 
+    let temperatureSection = document.querySelector('.temperature-section')
+
+    let temperatureSpan = document.querySelector('.degree-type')
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(pos => {
             long = pos.coords.longitude;
             lat = pos.coords.latitude;
             
-            const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
             // Solution from stack overflow
+            const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
             
             const apiToCall = `${proxyUrl}https://api.darksky.net/forecast/${key}/${lat},${long}`;
             // console.log(apiToCall);
@@ -30,9 +33,24 @@ window.addEventListener("load", (result) => {
                 temperatureDegree.textContent = temperature;
                 locationTimezone.textContent =  data.timezone; 
                 setIcons(icon, document.querySelector(".icon"))
+
+                let cel = (temperature - 32)* (5 / 9);
+                cel = Math.round(cel*100)/100;;
+
+                temperatureSection.addEventListener('click', () => {
+                    if(temperatureSpan.textContent === '°C'){
+                        temperatureSpan.textContent = '°F'
+                        temperatureDegree.textContent = temperature;
+                    }
+                    else{
+                        temperatureSpan.textContent = '°C'
+                        temperatureDegree.textContent = cel;
+                    }
+                })
             })
 
         })
+
 
         function setIcons(icon, iconID) {
             const skycons = new Skycons({color: "white"});
